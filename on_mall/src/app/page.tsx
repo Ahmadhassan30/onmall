634d1d244d1d1099fa206a87d7e995a4d1c5dbbc
Ahmad Hassan <ahmadhassan@apples-MacBook-Pro.local>
2025-08-28 16:28:21 +0500

    next js setup complete and boiler plate code added

---
+++ on_mall/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1 style={{ fontSize: "4rem", fontWeight: "bold", textAlign: "center"}}>OnMall</h1>
+    <p style={{  textAlign: "center" }}>An ecommerce store where you can buy anything!</p>
+   </div>
+  );
+}
